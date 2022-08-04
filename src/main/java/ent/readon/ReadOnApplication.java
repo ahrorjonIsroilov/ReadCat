d012386283addd765bf4d695b3f7223eed002900
ahrorjon <softincuz@gmail.com>
2022-08-05 03:17:43 +0500

    first commit

---
+++ src/main/java/ent/readon/ReadOnApplication.java
@@ -0,0 +1,13 @@
+package ent.readon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadOnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ent/readon/ReadOnApplicationTests.java
@@ -0,0 +1,13 @@
+package ent.readon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
